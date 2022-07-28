cc3a98b8bf1196420eb881fd528b8c58bf49be95
IgorSologub <sologub355@gmail.com>
2022-07-28 17:15:27 +0300

    gg11

--- src/main/java/constants/Constant.java
+++ src/main/java/constants/Constant.java
@@ -82,7 +82,9 @@ public class Constant {
                 "WeMeetToday.com",
                 "NaughtyDate",
                 "MeetWild",
-                "BigoFun"
+                "BigoFun",
+                "hot girls",
+                "Best dating worldwide"
         );
         public static final int NUMBER_OF_DOWNLOAD_APP_REG_BUTTONS = 5;
         public static final int NUMBER_OF_VISIT_SITE_REG_BUTTONS = 24;
