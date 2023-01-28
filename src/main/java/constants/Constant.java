f17fc71aeb74cbc3f37694cae4c6ef033c16447c
IgorSologub <sologub355@gmail.com>
2023-01-28 21:29:44 +0200

    gg21

--- src/main/java/constants/Constant.java
+++ src/main/java/constants/Constant.java
@@ -65,7 +65,7 @@ public class Constant {
                 "Women",
                 "Match"
         );
-        public static final int NUMBER_OF_GOOD_FOOTER_BUTTONS = 40;
+        public static final int NUMBER_OF_GOOD_FOOTER_BUTTONS = 39;
     }
 
     public static class Reg {
@@ -96,7 +96,8 @@ public class Constant {
                 "BeSexcited",
                 "My Dirty Neighbour",
                 "Online Hookups",
-                "naughty-hub.com"
+                "naughty-hub.com",
+                "Dream Singles"
         );
         public static final int NUMBER_OF_DOWNLOAD_APP_REG_BUTTONS = 5;
         public static final int NUMBER_OF_VISIT_SITE_REG_BUTTONS = 24;
